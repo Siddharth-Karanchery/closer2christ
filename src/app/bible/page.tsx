bff0c57a5ff66435b68dca28bd2540e572a50d36
Siddharth Karanchery <webdev.siddharth@gmail.com>
2024-02-16 23:42:21 +0530

    using api data for Bibles, Books and Chapters

--- src/app/bible/page.tsx
+++ src/app/bible/page.tsx
@@ -9,29 +9,85 @@ import styles from "./Bible.module.css";
 
 function Bible() {
   const [bibles, setBibles] = React.useState<any[]>([]);
+  const [selBible, setSelBible] = React.useState<string>("");
+  const [books, setBooks] = React.useState<any[]>([]);
+  const [selBook, setSelBook] = React.useState<string>("");
+  const [chapters, setChapters] = React.useState<any[]>([]);
+  const [selChapter, setSelChapter] = React.useState<string>("");
+  const [verses, setVerses] = React.useState<any[]>([]);
+  const [selVerse, setSelVerse] = React.useState<string>("");
   React.useEffect(() => {
     axios
       .get(`https://api.scripture.api.bible/v1/bibles`, {
         headers: { "api-key": "1bc71a5553447160556f92fa1b817af4" },
       })
       .then((res) => {
-        console.log("res.data: ", res.data.data);
+        console.log("bibles: ", res.data.data);
         setBibles(res.data.data);
       });
   }, []);
 
-  const bibleData = [
-    { label: "Bible 1", id: 1, dbId: 1 },
-    { label: "Bible 2", id: 2, dbId: 2 },
-    { label: "Bible 3", id: 3, dbId: 3 },
-    { label: "Bible 4", id: 4, dbId: 4 },
-    { label: "Bible 5", id: 5, dbId: 5 },
-  ];
+  React.useEffect(() => {
+    axios
+      .get(`https://api.scripture.api.bible/v1/bibles/${selBible}/books`, {
+        headers: { "api-key": "1bc71a5553447160556f92fa1b817af4" },
+      })
+      .then((res) => {
+        console.log("books: ", res.data.data);
+        setBooks(res.data.data);
+      });
+  }, [selBible]);
 
+  React.useEffect(() => {
+    axios
+      .get(
+        `https://api.scripture.api.bible/v1/bibles/${selBible}/books/${selBook}/chapters`,
+        {
+          headers: { "api-key": "1bc71a5553447160556f92fa1b817af4" },
+        }
+      )
+      .then((res) => {
+        console.log("Chapters: ", res.data.data);
+        setChapters(res.data.data);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selBook]);
+
+  // React.useEffect(() => {
+  //   axios
+  //     .get(
+  //       `https://api.scripture.api.bible//v1/bibles/${selBible}/chapters/${selChapter}/verses`,
+  //       {
+  //         headers: { "api-key": "1bc71a5553447160556f92fa1b817af4" },
+  //       }
+  //     )
+  //     .then((res) => {
+  //       console.log("Verses: ", res.data.data);
+  //       // setSections(res.data.data);
+  //     });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [selChapter]);
+
+  const handleBibleSelect = (e: any) => {
+    setSelBible(e.target.value);
+  };
+
+  const handleBookSelect = (e: any) => {
+    setSelBook(e.target.value);
+  };
+
+  const handleChapterSelect = (e: any) => {
+    console.log("e:", e.target);
+    setSelChapter(e.target.value);
+  };
   return (
     <Box className={styles.Bible}>
       <Box className={styles.Bible__filterPanel}>
-        <select className={styles.Bible__filterPanel__filter} title="test">
+        <select
+          className={styles.Bible__filterPanel__filter}
+          title="test"
+          onChange={handleBibleSelect}
+        >
           <option value="" disabled selected>
             <em>Select a bible</em>
           </option>
@@ -42,36 +98,44 @@ function Bible() {
               </option>
             ))}
         </select>
-        <select className={styles.Bible__filterPanel__filter} title="test">
+        <select
+          className={styles.Bible__filterPanel__filter}
+          title="test"
+          onChange={handleBookSelect}
+        >
           <option value="" disabled selected>
             <em>Select a Book</em>
           </option>
-          {bibleData.map((bible) => (
-            <option value={bible.id} key={bible.label}>
-              {bible.label}
+          {books.map((book) => (
+            <option value={book.id} key={book.name}>
+              {book.name}
             </option>
           ))}
         </select>
-        <select className={styles.Bible__filterPanel__filter} title="test">
+        <select
+          className={styles.Bible__filterPanel__filter}
+          onChange={handleChapterSelect}
+          title="test"
+        >
           <option value="" disabled selected>
             <em>Select a Chapter</em>
           </option>
-          {bibleData.map((bible) => (
-            <option value={bible.id} key={bible.label}>
-              {bible.label}
+          {chapters.map((chapter) => (
+            <option value={chapter.id} key={chapter.number}>
+              {chapter.number}
             </option>
           ))}
         </select>
-        <select className={styles.Bible__filterPanel__filter} title="test">
+        {/* <select className={styles.Bible__filterPanel__filter} title="test">
           <option value="" disabled selected>
             <em>Select a Verse</em>
           </option>
-          {bibleData.map((bible) => (
+          {sections.map((section) => (
             <option value={bible.id} key={bible.label}>
               {bible.label}
             </option>
           ))}
-        </select>
+        </select> */}
       </Box>
       Bible
     </Box>
