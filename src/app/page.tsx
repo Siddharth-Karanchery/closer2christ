77ec5acc73971274f7113d7227a04d10548ed12e
Siddharth Karanchery <webdev.siddharth@gmail.com>
2024-02-29 22:40:37 +0530

    made home page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,11 @@ import Votd from "./components/Votd/Votd";
 export default function Home() {
   return (
     <Box className={styles.Home}>
-      <Box>
-        <Image
-          src={
-            "https://images.unsplash.com/photo-1642022143908-fe7e3160a56e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          fill
-          alt="logo"
-        />
-      </Box>
+      <img
+        className={styles.Home__image}
+        src="https://images.unsplash.com/photo-1642022143908-fe7e3160a56e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="open bible"
+      />
       <Votd />
     </Box>
   );
